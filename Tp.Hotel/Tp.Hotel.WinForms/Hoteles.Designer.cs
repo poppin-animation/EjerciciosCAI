59ba6dc55b6fa37bad98ea1bf9e24d08fd5a4dc5
micasessa <micasessa@yahoo.com>
2021-11-15 23:48:46 -0300

    Creacion Form Hotel
    con sus atributos, botones y listas

---
+++ Tp.Hotel/Tp.Hotel.WinForms/Hoteles.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace Tp.Hotel.WinForms
+{
+    partial class Hoteles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this._lblIdHotel = new System.Windows.Forms.Label();
+            this._lblNombre = new System.Windows.Forms.Label();
+            this._lblDireccion = new System.Windows.Forms.Label();
+            this._lblEstrellas = new System.Windows.Forms.Label();
+            this._lblAmenities = new System.Windows.Forms.Label();
+            this._txtIdHotel = new System.Windows.Forms.TextBox();
+            this._txtNombre = new System.Windows.Forms.TextBox();
+            this._txtDireccion = new System.Windows.Forms.TextBox();
+            this._cmbEstrellas = new System.Windows.Forms.ComboBox();
+            this._grpBxAmenities = new System.Windows.Forms.GroupBox();
+            this._btnVolver = new System.Windows.Forms.Button();
+            this._btnLimpiar = new System.Windows.Forms.Button();
+            this._btnAgregar = new System.Windows.Forms.Button();
+            this._btnMostrar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PowderBlue;
+            this.panel1.Controls.Add(this._btnMostrar);
+            this.panel1.Controls.Add(this._btnAgregar);
+            this.panel1.Controls.Add(this._btnLimpiar);
+            this.panel1.Controls.Add(this._btnVolver);
+            this.panel1.Controls.Add(this._grpBxAmenities);
+            this.panel1.Controls.Add(this._cmbEstrellas);
+            this.panel1.Controls.Add(this._txtDireccion);
+            this.panel1.Controls.Add(this._txtNombre);
+            this.panel1.Controls.Add(this._txtIdHotel);
+            this.panel1.Controls.Add(this._lblAmenities);
+            this.panel1.Controls.Add(this._lblEstrellas);
+            this.panel1.Controls.Add(this._lblDireccion);
+            this.panel1.Controls.Add(this._lblNombre);
+            this.panel1.Controls.Add(this._lblIdHotel);
+            this.panel1.Location = new System.Drawing.Point(-4, -19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(817, 482);
+            this.panel1.TabIndex = 0;
+            // 
+            // _lblIdHotel
+            // 
+            this._lblIdHotel.AutoSize = true;
+            this._lblIdHotel.Location = new System.Drawing.Point(71, 94);
+            this._lblIdHotel.Name = "_lblIdHotel";
+            this._lblIdHotel.Size = new System.Drawing.Size(65, 20);
+            this._lblIdHotel.TabIndex = 0;
+            this._lblIdHotel.Text = "Id Hotel";
+            // 
+            // _lblNombre
+            // 
+            this._lblNombre.AutoSize = true;
+            this._lblNombre.Location = new System.Drawing.Point(71, 138);
+            this._lblNombre.Name = "_lblNombre";
+            this._lblNombre.Size = new System.Drawing.Size(65, 20);
+            this._lblNombre.TabIndex = 1;
+            this._lblNombre.Text = "Nombre";
+            // 
+            // _lblDireccion
+            // 
+            this._lblDireccion.AutoSize = true;
+            this._lblDireccion.Location = new System.Drawing.Point(71, 186);
+            this._lblDireccion.Name = "_lblDireccion";
+            this._lblDireccion.Size = new System.Drawing.Size(75, 20);
+            this._lblDireccion.TabIndex = 2;
+            this._lblDireccion.Text = "Direccion";
+            // 
+            // _lblEstrellas
+            // 
+            this._lblEstrellas.AutoSize = true;
+            this._lblEstrellas.Location = new System.Drawing.Point(71, 235);
+            this._lblEstrellas.Name = "_lblEstrellas";
+            this._lblEstrellas.Size = new System.Drawing.Size(70, 20);
+            this._lblEstrellas.TabIndex = 3;
+            this._lblEstrellas.Text = "Estrellas";
+            // 
+            // _lblAmenities
+            // 
+            this._lblAmenities.AutoSize = true;
+            this._lblAmenities.Location = new System.Drawing.Point(476, 71);
+            this._lblAmenities.Name = "_lblAmenities";
+            this._lblAmenities.Size = new System.Drawing.Size(79, 20);
+            this._lblAmenities.TabIndex = 4;
+            this._lblAmenities.Text = "Amenities";
+            // 
+            // _txtIdHotel
+            // 
+            this._txtIdHotel.Location = new System.Drawing.Point(149, 94);
+            this._txtIdHotel.Name = "_txtIdHotel";
+            this._txtIdHotel.Size = new System.Drawing.Size(108, 26);
+            this._txtIdHotel.TabIndex = 5;
+            // 
+            // _txtNombre
+            // 
+            this._txtNombre.Location = new System.Drawing.Point(149, 138);
+            this._txtNombre.Name = "_txtNombre";
+            this._txtNombre.Size = new System.Drawing.Size(108, 26);
+            this._txtNombre.TabIndex = 6;
+            // 
+            // _txtDireccion
+            // 
+            this._txtDireccion.Location = new System.Drawing.Point(149, 180);
+            this._txtDireccion.Name = "_txtDireccion";
+            this._txtDireccion.Size = new System.Drawing.Size(108, 26);
+            this._txtDireccion.TabIndex = 7;
+            // 
+            // _cmbEstrellas
+            // 
+            this._cmbEstrellas.FormattingEnabled = true;
+            this._cmbEstrellas.Location = new System.Drawing.Point(149, 227);
+            this._cmbEstrellas.Name = "_cmbEstrellas";
+            this._cmbEstrellas.Size = new System.Drawing.Size(108, 28);
+            this._cmbEstrellas.TabIndex = 8;
+            // 
+            // _grpBxAmenities
+            // 
+            this._grpBxAmenities.BackColor = System.Drawing.Color.MintCream;
+            this._grpBxAmenities.Location = new System.Drawing.Point(471, 94);
+            this._grpBxAmenities.Name = "_grpBxAmenities";
+            this._grpBxAmenities.Size = new System.Drawing.Size(295, 254);
+            this._grpBxAmenities.TabIndex = 9;
+            this._grpBxAmenities.TabStop = false;
+            this._grpBxAmenities.Text = "_lstAmenities";
+            // 
+            // _btnVolver
+            // 
+            this._btnVolver.Location = new System.Drawing.Point(16, 411);
+            this._btnVolver.Name = "_btnVolver";
+            this._btnVolver.Size = new System.Drawing.Size(114, 36);
+            this._btnVolver.TabIndex = 10;
+            this._btnVolver.Text = "Volver";
+            this._btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // _btnLimpiar
+            // 
+            this._btnLimpiar.Location = new System.Drawing.Point(209, 291);
+            this._btnLimpiar.Name = "_btnLimpiar";
+            this._btnLimpiar.Size = new System.Drawing.Size(114, 36);
+            this._btnLimpiar.TabIndex = 11;
+            this._btnLimpiar.Text = "Limpiar";
+            this._btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // _btnAgregar
+            // 
+            this._btnAgregar.Location = new System.Drawing.Point(75, 291);
+            this._btnAgregar.Name = "_btnAgregar";
+            this._btnAgregar.Size = new System.Drawing.Size(114, 36);
+            this._btnAgregar.TabIndex = 12;
+            this._btnAgregar.Text = "Agregar";
+            this._btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // _btnMostrar
+            // 
+            this._btnMostrar.Location = new System.Drawing.Point(652, 370);
+            this._btnMostrar.Name = "_btnMostrar";
+            this._btnMostrar.Size = new System.Drawing.Size(114, 36);
+            this._btnMostrar.TabIndex = 13;
+            this._btnMostrar.Text = "Mostrar";
+            this._btnMostrar.UseVisualStyleBackColor = true;
+            // 
+            // Hoteles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Hoteles";
+            this.Text = "Hoteles";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button _btnMostrar;
+        private System.Windows.Forms.Button _btnAgregar;
+        private System.Windows.Forms.Button _btnLimpiar;
+        private System.Windows.Forms.Button _btnVolver;
+        private System.Windows.Forms.GroupBox _grpBxAmenities;
+        private System.Windows.Forms.ComboBox _cmbEstrellas;
+        private System.Windows.Forms.TextBox _txtDireccion;
+        private System.Windows.Forms.TextBox _txtNombre;
+        private System.Windows.Forms.TextBox _txtIdHotel;
+        private System.Windows.Forms.Label _lblAmenities;
+        private System.Windows.Forms.Label _lblEstrellas;
+        private System.Windows.Forms.Label _lblDireccion;
+        private System.Windows.Forms.Label _lblNombre;
+        private System.Windows.Forms.Label _lblIdHotel;
+    }
+}
