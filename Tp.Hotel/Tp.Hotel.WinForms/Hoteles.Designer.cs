cfc60dd2cd4d4d7340d73d8d1d89d4dcdaa8560a
micasessa <micasessa@yahoo.com>
2021-11-16 17:42:34 -0300

    Modificacion diseño form Hotel
    Cambié la letra y agregué una imagen

--- Tp.Hotel/Tp.Hotel.WinForms/Hoteles.Designer.cs
+++ Tp.Hotel/Tp.Hotel.WinForms/Hoteles.Designer.cs
@@ -29,27 +29,31 @@ namespace Tp.Hotel.WinForms
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Hoteles));
             this.panel1 = new System.Windows.Forms.Panel();
-            this._lblIdHotel = new System.Windows.Forms.Label();
-            this._lblNombre = new System.Windows.Forms.Label();
-            this._lblDireccion = new System.Windows.Forms.Label();
-            this._lblEstrellas = new System.Windows.Forms.Label();
-            this._lblAmenities = new System.Windows.Forms.Label();
-            this._txtIdHotel = new System.Windows.Forms.TextBox();
-            this._txtNombre = new System.Windows.Forms.TextBox();
-            this._txtDireccion = new System.Windows.Forms.TextBox();
-            this._cmbEstrellas = new System.Windows.Forms.ComboBox();
-            this._btnVolver = new System.Windows.Forms.Button();
-            this._btnLimpiar = new System.Windows.Forms.Button();
-            this._btnAgregar = new System.Windows.Forms.Button();
-            this._btnMostrar = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this._btnMostrar = new System.Windows.Forms.Button();
+            this._btnAgregar = new System.Windows.Forms.Button();
+            this._btnLimpiar = new System.Windows.Forms.Button();
+            this._btnVolver = new System.Windows.Forms.Button();
+            this._cmbEstrellas = new System.Windows.Forms.ComboBox();
+            this._txtDireccion = new System.Windows.Forms.TextBox();
+            this._txtNombre = new System.Windows.Forms.TextBox();
+            this._txtIdHotel = new System.Windows.Forms.TextBox();
+            this._lblAmenities = new System.Windows.Forms.Label();
+            this._lblEstrellas = new System.Windows.Forms.Label();
+            this._lblDireccion = new System.Windows.Forms.Label();
+            this._lblNombre = new System.Windows.Forms.Label();
+            this._lblIdHotel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.PowderBlue;
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.comboBox1);
             this.panel1.Controls.Add(this._btnMostrar);
             this.panel1.Controls.Add(this._btnAgregar);
@@ -64,84 +68,45 @@ namespace Tp.Hotel.WinForms
             this.panel1.Controls.Add(this._lblDireccion);
             this.panel1.Controls.Add(this._lblNombre);
             this.panel1.Controls.Add(this._lblIdHotel);
-            this.panel1.Location = new System.Drawing.Point(-4, -19);
+            this.panel1.Location = new System.Drawing.Point(-12, -17);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(505, 482);
+            this.panel1.Size = new System.Drawing.Size(844, 574);
             this.panel1.TabIndex = 0;
             // 
-            // _lblIdHotel
+            // comboBox1
             // 
-            this._lblIdHotel.AutoSize = true;
-            this._lblIdHotel.Location = new System.Drawing.Point(71, 94);
-            this._lblIdHotel.Name = "_lblIdHotel";
-            this._lblIdHotel.Size = new System.Drawing.Size(65, 20);
-            this._lblIdHotel.TabIndex = 0;
-            this._lblIdHotel.Text = "Id Hotel";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(184, 279);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(108, 28);
+            this.comboBox1.TabIndex = 14;
             // 
-            // _lblNombre
+            // _btnMostrar
             // 
-            this._lblNombre.AutoSize = true;
-            this._lblNombre.Location = new System.Drawing.Point(71, 138);
-            this._lblNombre.Name = "_lblNombre";
-            this._lblNombre.Size = new System.Drawing.Size(65, 20);
-            this._lblNombre.TabIndex = 1;
-            this._lblNombre.Text = "Nombre";
+            this._btnMostrar.Location = new System.Drawing.Point(203, 338);
+            this._btnMostrar.Name = "_btnMostrar";
+            this._btnMostrar.Size = new System.Drawing.Size(114, 36);
+            this._btnMostrar.TabIndex = 13;
+            this._btnMostrar.Text = "Mostrar";
+            this._btnMostrar.UseVisualStyleBackColor = true;
             // 
-            // _lblDireccion
+            // _btnAgregar
             // 
-            this._lblDireccion.AutoSize = true;
-            this._lblDireccion.Location = new System.Drawing.Point(71, 186);
-            this._lblDireccion.Name = "_lblDireccion";
-            this._lblDireccion.Size = new System.Drawing.Size(75, 20);
-            this._lblDireccion.TabIndex = 2;
-            this._lblDireccion.Text = "Direccion";
+            this._btnAgregar.Location = new System.Drawing.Point(61, 338);
+            this._btnAgregar.Name = "_btnAgregar";
+            this._btnAgregar.Size = new System.Drawing.Size(114, 36);
+            this._btnAgregar.TabIndex = 12;
+            this._btnAgregar.Text = "Agregar";
+            this._btnAgregar.UseVisualStyleBackColor = true;
             // 
-            // _lblEstrellas
+            // _btnLimpiar
             // 
-            this._lblEstrellas.AutoSize = true;
-            this._lblEstrellas.Location = new System.Drawing.Point(71, 235);
-            this._lblEstrellas.Name = "_lblEstrellas";
-            this._lblEstrellas.Size = new System.Drawing.Size(70, 20);
-            this._lblEstrellas.TabIndex = 3;
-            this._lblEstrellas.Text = "Estrellas";
-            // 
-            // _lblAmenities
-            // 
-            this._lblAmenities.AutoSize = true;
-            this._lblAmenities.Location = new System.Drawing.Point(67, 284);
-            this._lblAmenities.Name = "_lblAmenities";
-            this._lblAmenities.Size = new System.Drawing.Size(79, 20);
-            this._lblAmenities.TabIndex = 4;
-            this._lblAmenities.Text = "Amenities";
-            // 
-            // _txtIdHotel
-            // 
-            this._txtIdHotel.Location = new System.Drawing.Point(149, 94);
-            this._txtIdHotel.Name = "_txtIdHotel";
-            this._txtIdHotel.Size = new System.Drawing.Size(108, 26);
-            this._txtIdHotel.TabIndex = 5;
-            // 
-            // _txtNombre
-            // 
-            this._txtNombre.Location = new System.Drawing.Point(149, 138);
-            this._txtNombre.Name = "_txtNombre";
-            this._txtNombre.Size = new System.Drawing.Size(108, 26);
-            this._txtNombre.TabIndex = 6;
-            // 
-            // _txtDireccion
-            // 
-            this._txtDireccion.Location = new System.Drawing.Point(149, 180);
-            this._txtDireccion.Name = "_txtDireccion";
-            this._txtDireccion.Size = new System.Drawing.Size(108, 26);
-            this._txtDireccion.TabIndex = 7;
-            // 
-            // _cmbEstrellas
-            // 
-            this._cmbEstrellas.FormattingEnabled = true;
-            this._cmbEstrellas.Location = new System.Drawing.Point(149, 227);
-            this._cmbEstrellas.Name = "_cmbEstrellas";
-            this._cmbEstrellas.Size = new System.Drawing.Size(108, 28);
-            this._cmbEstrellas.TabIndex = 8;
+            this._btnLimpiar.Location = new System.Drawing.Point(451, 419);
+            this._btnLimpiar.Name = "_btnLimpiar";
+            this._btnLimpiar.Size = new System.Drawing.Size(114, 36);
+            this._btnLimpiar.TabIndex = 11;
+            this._btnLimpiar.Text = "Limpiar";
+            this._btnLimpiar.UseVisualStyleBackColor = true;
             // 
             // _btnVolver
             // 
@@ -152,51 +117,105 @@ namespace Tp.Hotel.WinForms
             this._btnVolver.Text = "Volver";
             this._btnVolver.UseVisualStyleBackColor = true;
             // 
-            // _btnLimpiar
+            // _cmbEstrellas
             // 
-            this._btnLimpiar.Location = new System.Drawing.Point(265, 411);
-            this._btnLimpiar.Name = "_btnLimpiar";
-            this._btnLimpiar.Size = new System.Drawing.Size(114, 36);
-            this._btnLimpiar.TabIndex = 11;
-            this._btnLimpiar.Text = "Limpiar";
-            this._btnLimpiar.UseVisualStyleBackColor = true;
+            this._cmbEstrellas.FormattingEnabled = true;
+            this._cmbEstrellas.Location = new System.Drawing.Point(184, 230);
+            this._cmbEstrellas.Name = "_cmbEstrellas";
+            this._cmbEstrellas.Size = new System.Drawing.Size(108, 28);
+            this._cmbEstrellas.TabIndex = 8;
             // 
-            // _btnAgregar
+            // _txtDireccion
             // 
-            this._btnAgregar.Location = new System.Drawing.Point(71, 338);
-            this._btnAgregar.Name = "_btnAgregar";
-            this._btnAgregar.Size = new System.Drawing.Size(114, 36);
-            this._btnAgregar.TabIndex = 12;
-            this._btnAgregar.Text = "Agregar";
-            this._btnAgregar.UseVisualStyleBackColor = true;
+            this._txtDireccion.Location = new System.Drawing.Point(184, 183);
+            this._txtDireccion.Name = "_txtDireccion";
+            this._txtDireccion.Size = new System.Drawing.Size(108, 26);
+            this._txtDireccion.TabIndex = 7;
             // 
-            // _btnMostrar
+            // _txtNombre
             // 
-            this._btnMostrar.Location = new System.Drawing.Point(213, 338);
-            this._btnMostrar.Name = "_btnMostrar";
-            this._btnMostrar.Size = new System.Drawing.Size(114, 36);
-            this._btnMostrar.TabIndex = 13;
-            this._btnMostrar.Text = "Mostrar";
-            this._btnMostrar.UseVisualStyleBackColor = true;
+            this._txtNombre.Location = new System.Drawing.Point(184, 138);
+            this._txtNombre.Name = "_txtNombre";
+            this._txtNombre.Size = new System.Drawing.Size(108, 26);
+            this._txtNombre.TabIndex = 6;
             // 
-            // comboBox1
+            // _txtIdHotel
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(149, 276);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(108, 28);
-            this.comboBox1.TabIndex = 14;
+            this._txtIdHotel.Location = new System.Drawing.Point(184, 94);
+            this._txtIdHotel.Name = "_txtIdHotel";
+            this._txtIdHotel.Size = new System.Drawing.Size(108, 26);
+            this._txtIdHotel.TabIndex = 5;
+            // 
+            // _lblAmenities
+            // 
+            this._lblAmenities.AutoSize = true;
+            this._lblAmenities.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this._lblAmenities.Location = new System.Drawing.Point(71, 284);
+            this._lblAmenities.Name = "_lblAmenities";
+            this._lblAmenities.Size = new System.Drawing.Size(107, 23);
+            this._lblAmenities.TabIndex = 4;
+            this._lblAmenities.Text = "Amenities";
+            // 
+            // _lblEstrellas
+            // 
+            this._lblEstrellas.AutoSize = true;
+            this._lblEstrellas.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this._lblEstrellas.Location = new System.Drawing.Point(71, 235);
+            this._lblEstrellas.Name = "_lblEstrellas";
+            this._lblEstrellas.Size = new System.Drawing.Size(86, 23);
+            this._lblEstrellas.TabIndex = 3;
+            this._lblEstrellas.Text = "Estrellas";
+            // 
+            // _lblDireccion
+            // 
+            this._lblDireccion.AutoSize = true;
+            this._lblDireccion.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this._lblDireccion.Location = new System.Drawing.Point(71, 186);
+            this._lblDireccion.Name = "_lblDireccion";
+            this._lblDireccion.Size = new System.Drawing.Size(104, 23);
+            this._lblDireccion.TabIndex = 2;
+            this._lblDireccion.Text = "Direccion";
+            // 
+            // _lblNombre
+            // 
+            this._lblNombre.AutoSize = true;
+            this._lblNombre.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this._lblNombre.Location = new System.Drawing.Point(71, 138);
+            this._lblNombre.Name = "_lblNombre";
+            this._lblNombre.Size = new System.Drawing.Size(89, 23);
+            this._lblNombre.TabIndex = 1;
+            this._lblNombre.Text = "Nombre";
+            // 
+            // _lblIdHotel
+            // 
+            this._lblIdHotel.AutoSize = true;
+            this._lblIdHotel.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._lblIdHotel.Location = new System.Drawing.Point(71, 94);
+            this._lblIdHotel.Name = "_lblIdHotel";
+            this._lblIdHotel.Size = new System.Drawing.Size(86, 23);
+            this._lblIdHotel.TabIndex = 0;
+            this._lblIdHotel.Text = "Id Hotel";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(318, 109);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(247, 198);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
             // 
             // Hoteles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 450);
+            this.ClientSize = new System.Drawing.Size(574, 450);
             this.Controls.Add(this.panel1);
             this.Name = "Hoteles";
             this.Text = "Hoteles";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -218,5 +237,6 @@ namespace Tp.Hotel.WinForms
         private System.Windows.Forms.Label _lblNombre;
         private System.Windows.Forms.Label _lblIdHotel;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
