f489ab3fc9cccf25f61e0df212973661622149ce
micasessa <micasessa@yahoo.com>
2021-11-15 23:07:59 -0300

    Creacion form Habitacion

---
+++ Tp.Hotel/Tp.Hotel.WinForms/FrmHabitacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tp.Hotel.WinForms
+{
+    public partial class FrmHabitacion : Form
+    {
+        public FrmHabitacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
